0622cac804f174df55bc268048c860285f699657
NetSpeed Dev <dev@netspeed.test>
2026-06-16 07:44:56 +0000

    fix: increase gradle memory to 2GB, remove material-icons-extended

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("androidx.navigation:navigation-compose:2.7.5")
 
